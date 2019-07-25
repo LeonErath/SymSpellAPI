251d5e8e37e8cf9b7e695f01cb67c3f6a3db1ad9
Leon Erath <leon-erath@hotmail.de>
2019-07-25 14:42:06 +0200

    added JSON support

--- SymSpellAPI/Controllers/APIController.cs
+++ SymSpellAPI/Controllers/APIController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace SymSpellAPI.Controllers
 {
+
+    
+
     [Route("/[controller]")]
     [ApiController]
     public class APIController : ControllerBase
@@ -17,8 +22,31 @@ namespace SymSpellAPI.Controllers
             return  "API Initialited";
         }
 
+        [HttpPost("lookup/json")]
+        public List<SymSpell.SuggestItem> lookupJSON([FromBody]JObject data)
+        {
+            Console.WriteLine("lookup/json " + data.GetValue("document").Value<string>() +" "+ data.GetValue("distance").Value<int>()+ " "+ data.GetValue("verbosity").Value<int>());
+            return SymSpellInterface.Instance.getSuggestions(data.GetValue("document").Value<string>(),  data.GetValue("verbosity").Value<int>(), data.GetValue("distance").Value<int>());
+        }
 
-        [HttpPost("lookup")]
+        
+
+        [HttpPost("comound/json")]
+        public List<SymSpell.SuggestItem> lookupcompoundJSON([FromBody]JObject data)
+        {
+            Console.WriteLine("compound/json - document: " + data.GetValue("document").Value<string>());
+            return SymSpellInterface.Instance.correctText(data.GetValue("document").Value<string>(), data.GetValue("distance").Value<int>());
+        }
+
+        [HttpPost("stemming/json")]
+        public (string segmentedString, string correctedString, int distanceSum, decimal probabilityLogSum) wordStemmingJSON([FromBody]JObject data)
+        {
+            Console.WriteLine("stemming/json - document: " + data.GetValue("document").Value<string>());
+            return SymSpellInterface.Instance.segmentText(data.GetValue("document").Value<string>(), data.GetValue("distance").Value<int>());
+        }
+    
+
+        [HttpPost("lookup/formdata")]
         public ActionResult<List<SymSpell.SuggestItem>> lookup()
         {
             string document = Request.Form["document"];
@@ -30,7 +58,7 @@ namespace SymSpellAPI.Controllers
 
         }
 
-        [HttpPost("lookupcompound")]
+        [HttpPost("compound/formdata")]
         public ActionResult<List<SymSpell.SuggestItem>> lookupcompound()
         {
             string document = Request.Form["document"];
@@ -39,7 +67,7 @@ namespace SymSpellAPI.Controllers
             return SymSpellInterface.Instance.correctText(document,distance);
         }
 
-        [HttpPost("wordstemming")]
+        [HttpPost("stemming/formdata")]
         public ActionResult<(string segmentedString, string correctedString, int distanceSum, decimal probabilityLogSum)> wordstemming()
         {
             string document = Request.Form["document"];
